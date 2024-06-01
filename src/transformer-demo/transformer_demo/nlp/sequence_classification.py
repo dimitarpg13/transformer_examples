90761011ca768250c1c1e4fd0a6eb59310c15b30
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-31 22:01:45 -0400

    update module

--- src/transformer-demo/transformer_demo/nlp/sequence_classification.py
+++ src/transformer-demo/transformer_demo/nlp/sequence_classification.py
@@ -1,2 +1,21 @@
 from datasets import load_dataset
-imdb = load_dataset("imdb")
+from transformers import AutoTokenizer, DataCollatorWithPadding
+
+
+def main():
+    imdb = load_dataset("imdb")
+    print(imdb["test"][0])
+
+    tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+
+    def preprocess_function(examples):
+        return tokenizer(examples["text"], truncation=True)
+
+    tokenized_imdb = imdb.map(preprocess_function, batched=True)
+    data_collator = DataCollatorWithPadding(tokenizer)
+
+    # TODO: finish the script
+
+
+if __name__ == "__main__":
+    main()
